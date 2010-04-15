a7b3b3ddb6566489d56075bac03e48be93ad00f2
NIA <nia.fti@mail.ru>
2010-04-16 02:17:53 +0700

    13 - dekanat

--- contacts.sql
+++ contacts.sql
@@ -6,13 +6,13 @@
 /* ********************************** */
 
 CREATE TABLE contacts (
-    id          INTEGER NOT NULL PRIMARY KEY,
-    first_name  VARCHAR2(50),
-    last_name   VARCHAR2(50),
-    email       VARCHAR2(30),
-    zipcode     VARCHAR2(6),
-    city        VARCHAR2(25),
-    street      VARCHAR2(40),
-    building    VARCHAR2(15),
-    apartment   VARCHAR2(5)
+  id          INTEGER NOT NULL PRIMARY KEY,
+  first_name  VARCHAR2(50),
+  last_name   VARCHAR2(50),
+  email       VARCHAR2(30),
+  zipcode     VARCHAR2(6),
+  city        VARCHAR2(25),
+  street      VARCHAR2(40),
+  building    VARCHAR2(15),
+  apartment   VARCHAR2(5)
 );
---
+++ dekanat.sql
@@ -0,0 +1,52 @@
+/* ********************************** */
+/*          Task 13, 'Dekanat'       */
+/*              Ivan Novikov          */
+/* Relational Databases Basics course */
+/*           AFTI NSU, 2010           */
+/* ********************************** */
+
+CREATE TABLE teachers (
+  id          INTEGER NOT NULL PRIMARY KEY,
+  first_name  VARCHAR2(30),
+  middle_name VARCHAR2(30),
+  last_name   VARCHAR2(40),
+  email       VARCHAR2(30),
+  phone       VARCHAR2(20),
+  room        VARCHAR2(5)
+);
+
+CREATE TABLE students (
+  id          INTEGER NOT NULL PRIMARY KEY,
+  first_name  VARCHAR2(30),
+  middle_name VARCHAR2(30),
+  last_name   VARCHAR2(40),
+  email       VARCHAR2(30),
+  phone       VARCHAR2(20),
+  dorm_number INTEGER,
+  dorm_room   VARCHAR2(5)
+);
+
+CREATE TABLE courses (
+  code        INTEGER NOT NULL PRIMARY KEY,
+  name        VARCHAR2(20),
+  hours       INTEGER
+);
+
+CREATE TABLE year_courses (
+  id          INTEGER NOT NULL PRIMARY KEY,
+  course_code INTEGER NOT NULL REFERENCE courses(id),
+  date_start  DATE
+);
+
+CREATE TABLE teaching (
+  teacher_id  INTEGER NOT NULL REFERENCE teachers(id),
+  yc_id       INTEGER NOT NULL REFERENCE year_courses(id)
+);
+
+CREATE TABLE studying (
+  student_id  INTEGER NOT NULL REFERENCE students(id),
+  yc_id       INTEGER NOT NULL REFERENCE year_courses(id),
+  grade       CHAR
+  CONSTRAINT  pk_studying PRIMARY KEY(student_id, yc_id)
+);
+  
