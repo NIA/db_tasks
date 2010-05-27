7ee5c97265e65e3e7733b2aff6c518149f2bc62e
NIA <nia.fti@mail.ru>
2010-05-28 02:50:59 +0700

    40 (simplifications)

--- shop/40.dropall.sql
+++ shop/40.dropall.sql
@@ -5,9 +5,7 @@ declare
     select * from user_constraints uc
     where uc.table_name not in
       (select object_name from user_recyclebin);
-  cursor triggers is
-    select * from user_triggers;
-  cursor objects is
+  cursor all_but_tables is
     select * from user_objects
     where user_objects.object_name not in
       (select object_name from user_recyclebin)
@@ -22,12 +20,7 @@ begin
     dbms_output.put_line('alter table "'||c.table_name||'" drop constraint "'||c.constraint_name || '"');
     execute immediate 'alter table "'||c.table_name||'" drop constraint "' || c.constraint_name || '"';
   end loop;
-  for t in triggers
-  loop
-    dbms_output.put_line('drop trigger "'||t.trigger_name||'"');
-    execute immediate 'drop trigger "'||t.trigger_name||'"';
-  end loop;
-  for o in objects
+  for o in all_but_tables
   loop
     dbms_output.put_line('drop '||o.object_type||' "'||o.object_name||'"');
     execute immediate 'drop '||o.object_type||' "'||o.object_name||'"';
